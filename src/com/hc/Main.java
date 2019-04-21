ad579c9fa9de9e58633c90cf862c97be3b3ebc88
wo <1554125475@qq.com>
2019-04-21 20:26:29 +0800

    testInternet

--- src/com/hc/Main.java
+++ src/com/hc/Main.java
@@ -1,9 +1,17 @@
 package com.hc;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        executorService.isShutdown();
     }
+
 }
